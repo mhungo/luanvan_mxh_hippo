bc1325310b0edf593e222c815b5933b3e327ac9a
Hung <phamhung2015cb@gmail.com>
2021-05-19 10:44:39 +0700

    follow screen

--- app/src/main/java/vn/edu/stu/luanvanmxhhippo/FollowersActivity.java
+++ app/src/main/java/vn/edu/stu/luanvanmxhhippo/FollowersActivity.java
@@ -1,14 +1,214 @@
 package vn.edu.stu.luanvanmxhhippo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.edu.stu.Adapter.UserAdapter;
+import vn.edu.stu.Model.User;
 
 public class FollowersActivity extends AppCompatActivity {
+
+    private String id;
+    private String title;
+
+    private List<String> idList;
+
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<User> userList;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_followers);
+
+        addControls();
+        getDataIntent();
+        addEvents();
+    }
+
+    private void addEvents() {
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        switch (title) {
+            case "likes":
+                getLikes();
+                break;
+            case "following":
+                getFollowing();
+                break;
+            case "followers":
+                getFollowers();
+                break;
+
+            case "views":
+                getViews();
+                break;
+        }
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private void getViews() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Story")
+                .child(id).child(getIntent().getStringExtra("storyid")).child("views");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    idList.add(dataSnapshot.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getFollowing() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Follow")
+                .child(id).child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    idList.add(dataSnapshot.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getFollowers() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Follow")
+                .child(id).child("followers");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    idList.add(dataSnapshot.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getLikes() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Likes")
+                .child(id);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    idList.add(dataSnapshot.getKey());
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void showUsers() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    for (String id : idList) {
+                        if (user.getId().equals(id)) {
+                            userList.add(user);
+                        }
+                    }
+                }
+                userAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getDataIntent() {
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        title = intent.getStringExtra("title");
+
+    }
+
+    private void addControls() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        userList = new ArrayList<>();
+        userAdapter = new UserAdapter(this, userList, false);
+        recyclerView.setAdapter(userAdapter);
+
+        idList = new ArrayList<>();
+
     }
 }
