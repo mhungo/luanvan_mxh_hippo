27eee2125299c311c13e298b43324a324f31de25
Hung <phamhung2015cb@gmail.com>
2021-05-19 09:44:18 +0700

    chat manager

--- app/src/main/java/vn/edu/stu/luanvanmxhhippo/ChatManagerActivity.java
+++ app/src/main/java/vn/edu/stu/luanvanmxhhippo/ChatManagerActivity.java
@@ -1,14 +1,42 @@
 package vn.edu.stu.luanvanmxhhippo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import vn.edu.stu.Adapter.SectionsPagerAdapter;
+
 public class ChatManagerActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager viewPager;
+    private SectionsPagerAdapter sectionsPagerAdapter;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_manager);
+
+        mToolbar = findViewById(R.id.toobar);
+        setSupportActionBar(mToolbar);
+
+        viewPager = findViewById(R.id.main_tab_pager);
+        sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        mTabLayout = findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
