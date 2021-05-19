0ddd768e16a54810f9a61b2737ef2b3cc17cfcc5
Hung <phamhung2015cb@gmail.com>
2021-05-19 09:24:26 +0700

    Comment - read Comment

--- app/src/main/java/vn/edu/stu/luanvanmxhhippo/CommentsActivity.java
+++ app/src/main/java/vn/edu/stu/luanvanmxhhippo/CommentsActivity.java
@@ -1,14 +1,196 @@
 package vn.edu.stu.luanvanmxhhippo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import vn.edu.stu.Adapter.CommentAdapter;
+import vn.edu.stu.Model.Comment;
+import vn.edu.stu.Model.User;
 
 public class CommentsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private CommentAdapter commentAdapter;
+    private List<Comment> commentList;
+
+    private EditText addcomment;
+    private ImageView image_profile;
+    private TextView post;
+
+    private CircularProgressIndicator progressBar;
+
+    private String postid;
+    private String publisherid;
+
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        addControls();
+        addEvents();
+
+        getImage();
+        readcomments();
     }
+
+    private void addEvents() {
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (addcomment.getText().toString().equals("")) {
+                    Toast.makeText(CommentsActivity.this, "You can't send comment", Toast.LENGTH_SHORT).show();
+                } else {
+                    addComment();
+                    //backgroundAddComment.start();
+                }
+            }
+        });
+
+    }
+
+    private void addComment() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Comments").child(postid);
+
+        String commentid = reference.push().getKey();
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("comment", addcomment.getText().toString());
+        hashMap.put("publisher", firebaseUser.getUid());
+        hashMap.put("commentid", commentid);
+
+        reference.child(commentid).setValue(hashMap);
+        //backgroundAddNotification.start();
+        addNotifications();
+        addcomment.setText("");
+
+    }
+
+    private void addNotifications() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Notifications").child(publisherid);
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("userid", firebaseUser.getUid());
+        hashMap.put("text", "commented: " + addcomment.getText().toString());
+        hashMap.put("postid", postid);
+        hashMap.put("ispost", true);
+        reference.push().setValue(hashMap);
+        addcomment.setText("");
+    }
+
+    private void getImage() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if (user != null) {
+                    Glide.with(getApplicationContext()).load(user.getImageurl()).into(image_profile);
+                } else {
+                    image_profile.setImageResource(R.drawable.placeholder);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void readcomments() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Comments").child(postid);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                commentList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Comment comment = dataSnapshot.getValue(Comment.class);
+                    commentList.add(comment);
+                }
+                commentAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void addControls() {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Comments");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        progressBar = findViewById(R.id.progress_bar);
+
+        Intent intent = getIntent();
+        postid = intent.getStringExtra("postid");
+        publisherid = intent.getStringExtra("publisherid");
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        commentList = new ArrayList<>();
+        commentAdapter = new CommentAdapter(this, commentList, postid);
+        recyclerView.setAdapter(commentAdapter);
+
+        addcomment = findViewById(R.id.add_comment);
+        image_profile = findViewById(R.id.image_profile);
+        post = findViewById(R.id.post);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
 }
