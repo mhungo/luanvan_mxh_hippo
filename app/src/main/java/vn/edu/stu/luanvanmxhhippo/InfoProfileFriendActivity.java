cc2bda7447ac293a692e4f631fbdf5f2f320ac24
Hung <phamhung2015cb@gmail.com>
2021-06-10 22:08:12 +0700

    change layout info user

--- app/src/main/java/vn/edu/stu/luanvanmxhhippo/InfoProfileFriendActivity.java
+++ app/src/main/java/vn/edu/stu/luanvanmxhhippo/InfoProfileFriendActivity.java
@@ -1,66 +1,34 @@
 package vn.edu.stu.luanvanmxhhippo;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.google.android.material.progressindicator.CircularProgressIndicator;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.material.button.MaterialButton;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-
-import vn.edu.stu.Adapter.MyFotoAdapter;
-import vn.edu.stu.Model.Post;
-import vn.edu.stu.Model.User;
-
-import static java.security.AccessController.getContext;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class InfoProfileFriendActivity extends AppCompatActivity {
 
-    private ImageView image_profile, option, back;
-    private TextView posts, followers, followings, fullname, bio, username;
-    private Button edit_profile;
+    private String profileid = "";
+    private ImageView imageViewBack, image_background;
+    private TextView username, fullname, total_friend, mutual_friends;
 
-    private List<String> mySaves;
+    private LinearLayout linearLayout_add_friend, linearLayout_request_friend, linearLayout_friend;
 
-    private RecyclerView recyclerView_saves;
-    private MyFotoAdapter myFotoAdapter_saves;
-    private List<Post> postList_saves;
+    private CircleImageView image_profile;
+    private MaterialButton btn_friend, btn_chat_friend_layout, btn_more_friend_layout,
+            btn_request_friend, btn_chat_friend_request_layout, btn_more_request_layout,
+            btn_add_friend, btn_chat_friend, btn_follow_friend;
 
-    private RecyclerView recyclerView;
-    private MyFotoAdapter myFotoAdapter;
-    private List<Post> postList;
+    private RecyclerView recycler_view_post, recycler_view_mutual_friend;
 
-
-    private FirebaseUser firebaseUser;
-    private String profileid;
-
-    private ImageView my_fotos, saved_fotos;
-
-    private CircularProgressIndicator progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,223 +39,45 @@ public class InfoProfileFriendActivity extends AppCompatActivity {
         getDataIntent();
         addEvent();
 
-        //Chay da luong lay thong tin user
-        backgroundGetInfoUser.start();
     }
 
+    //get intent data
     private void getDataIntent() {
         SharedPreferences prefs = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
         profileid = prefs.getString("profileid", "none");
     }
 
     private void addControls() {
-
-        progressBar = findViewById(R.id.progress_bar);
-
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        image_profile = findViewById(R.id.image_profile);
-        option = findViewById(R.id.options);
-        back = findViewById(R.id.back);
-        posts = findViewById(R.id.posts);
-        followers = findViewById(R.id.followers);
-        followings = findViewById(R.id.following);
-        fullname = findViewById(R.id.fullname);
-        bio = findViewById(R.id.bio);
+        imageViewBack = findViewById(R.id.back);
+        image_background = findViewById(R.id.image_background);
         username = findViewById(R.id.username);
-        edit_profile = findViewById(R.id.edit_profile);
-        my_fotos = findViewById(R.id.my_fotos);
-        saved_fotos = findViewById(R.id.save_fotos);
+        fullname = findViewById(R.id.fullname);
+        total_friend = findViewById(R.id.total_friend);
+        mutual_friends = findViewById(R.id.mutual_friends);
+        linearLayout_add_friend = findViewById(R.id.layout_add_friend);
+        linearLayout_request_friend = findViewById(R.id.layout_request_friend);
+        linearLayout_friend = findViewById(R.id.layout_friend);
+        image_profile = findViewById(R.id.image_profile);
+        btn_friend = findViewById(R.id.btn_friend);
+        btn_chat_friend_layout = findViewById(R.id.btn_chat_friend_layout);
+        btn_more_friend_layout = findViewById(R.id.btn_more_friend_layout);
+        btn_request_friend = findViewById(R.id.btn_request_friend);
+        btn_chat_friend_request_layout = findViewById(R.id.btn_chat_friend_request_layout);
+        btn_more_request_layout = findViewById(R.id.btn_more_request_layout);
+        btn_add_friend = findViewById(R.id.btn_add_friend);
+        btn_chat_friend = findViewById(R.id.btn_chat_friend);
+        btn_follow_friend = findViewById(R.id.btn_follow_friend);
 
-        recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager = new GridLayoutManager(InfoProfileFriendActivity.this, 3);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        postList = new ArrayList<>();
-        myFotoAdapter = new MyFotoAdapter(InfoProfileFriendActivity.this, postList);
-        recyclerView.setAdapter(myFotoAdapter);
+        recycler_view_post = findViewById(R.id.recycler_view_post);
+        recycler_view_mutual_friend = findViewById(R.id.recycler_view_mutual_friend);
 
-        recyclerView_saves = findViewById(R.id.recycler_view_save);
-        recyclerView_saves.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager_save = new GridLayoutManager(InfoProfileFriendActivity.this, 3);
-        recyclerView_saves.setLayoutManager(linearLayoutManager_save);
-        postList_saves = new ArrayList<>();
-        myFotoAdapter_saves = new MyFotoAdapter(InfoProfileFriendActivity.this, postList_saves);
-        recyclerView_saves.setAdapter(myFotoAdapter_saves);
 
-        recyclerView_saves.setVisibility(View.VISIBLE);
-        recyclerView_saves.setVisibility(View.GONE);
     }
 
     private void addEvent() {
-        edit_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String btn = edit_profile.getText().toString();
-
-                if (btn.equals("Edit Profile")) {
-                    startActivity(new Intent(InfoProfileFriendActivity.this, EditProfileActivity.class));
-
-                } else if (btn.equals("follow")) {
-                    FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid())
-                            .child("following").child(profileid).setValue(true);
-                    FirebaseDatabase.getInstance().getReference().child("Follow").child(profileid)
-                            .child("followers").child(firebaseUser.getUid()).setValue(true);
-                    addNotifications();
-
-                } else if (btn.equals("following")) {
-                    FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid())
-                            .child("following").child(profileid).removeValue();
-                    FirebaseDatabase.getInstance().getReference().child("Follow").child(profileid)
-                            .child("followers").child(firebaseUser.getUid()).removeValue();
-                }
-
-            }
-        });
-
-        option.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(InfoProfileFriendActivity.this, OptionsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-        my_fotos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                recyclerView.setVisibility(View.VISIBLE);
-                recyclerView_saves.setVisibility(View.GONE);
-            }
-        });
-
-        saved_fotos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                recyclerView.setVisibility(View.GONE);
-                recyclerView_saves.setVisibility(View.VISIBLE);
-            }
-        });
-
-        followers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(InfoProfileFriendActivity.this, FollowersActivity.class);
-                intent.putExtra("id", profileid);
-                intent.putExtra("title", "followers");
-                startActivity(intent);
-            }
-        });
-
-        followings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(InfoProfileFriendActivity.this, FollowersActivity.class);
-                intent.putExtra("id", profileid);
-                intent.putExtra("title", "following");
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void addNotifications() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Notifications").child(profileid);
-
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("userid", firebaseUser.getUid());
-        hashMap.put("text", "Started following you");
-        hashMap.put("postid", "");
-        hashMap.put("ispost", false);
-
-        reference.push().setValue(hashMap);
-    }
-
-    private void userInfo() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(profileid);
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (getContext() == null) {
-                    return;
-                }
-                //Kiem tra user null
-                User user = snapshot.getValue(User.class);
-                if (user == null) {
-                    edit_profile.setVisibility(View.GONE);
-                    return;
-                } else {
-                    //set image user
-                    try {
-                        Glide.with(InfoProfileFriendActivity.this).load(user.getImageurl())
-                                .placeholder(R.drawable.placeholder)
-                                .into(image_profile);
-                    } catch (Exception e) {
-                        image_profile.setImageResource(R.drawable.placeholder);
-                    }
-
-                    //set username, full name ,....
-                    username.setText(user.getUsername());
-                    fullname.setText(user.getFullname());
-                    bio.setText(user.getBio());
-
-                    //check btn Edit/ follow
-                    if (profileid.equals(firebaseUser.getUid())) {
-                        edit_profile.setText("Edit Profile");
-                    } else {
-                        checkFollow();
-                        //visible save post
-                        saved_fotos.setVisibility(View.GONE);
-                    }
-                    getFollowers();
-                    getNrPost();
-                    myFotos();
-                    mysaves();
-                }
-                progressBar.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
 
     }
 
-    private void checkFollow() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
-                .child("Follow").child(firebaseUser.getUid()).child("following");
-
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                if (snapshot.child(profileid).exists()) {
-                    edit_profile.setText("following");
-                } else {
-                    edit_profile.setText("follow");
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    Thread backgroundGetInfoUser = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            userInfo();
-        }
-    });
 
     @Override
     public void onBackPressed() {
@@ -295,131 +85,4 @@ public class InfoProfileFriendActivity extends AppCompatActivity {
         finish();
     }
 
-    private void getFollowers() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Follow")
-                .child(profileid).child("followers");
-
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                followers.setText("" + snapshot.getChildrenCount());
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
-
-        DatabaseReference referencef = FirebaseDatabase.getInstance().getReference("Follow")
-                .child(profileid).child("following");
-
-        referencef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                followings.setText("" + snapshot.getChildrenCount());
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    private void getNrPost() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                int i = 0;
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    Post post = dataSnapshot.getValue(Post.class);
-
-                    if (post.getPublisher().equals(profileid)) {
-                        i++;
-                    }
-                }
-                posts.setText("" + i);
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    private void myFotos() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                postList.clear();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    Post post = dataSnapshot.getValue(Post.class);
-
-                    if (post.getPublisher().equals(profileid)) {
-                        postList.add(post);
-                    }
-                    Collections.reverse(postList);
-                    myFotoAdapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    private void mysaves() {
-        mySaves = new ArrayList<>();
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Saves")
-                .child(firebaseUser.getUid());
-
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                mySaves.clear();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    mySaves.add(dataSnapshot.getKey());
-                }
-                /*  Collections.reverse(mySaves);*/
-                readSaves();
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    private void readSaves() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                postList_saves.clear();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    Post post = dataSnapshot.getValue(Post.class);
-
-                    for (String id : mySaves) {
-                        if (post.getPostid().equals(id)) {
-                            postList_saves.add(post);
-                        }
-                    }
-                }
-                myFotoAdapter_saves.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-
-            }
-        });
-
-    }
 }
